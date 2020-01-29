63bf6a33770ff0792432ae72bb4e0dd95c6f7089
ci-build <rosswilsonblair+niworkflows-circleci@gmail.com>
2020-01-29 02:18:14 +0000

    doc(update) [skip ci]

--- maint_1.1.x/_static/documentation_options.js
+++ maint_1.1.x/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1.3+4.g4e65236f',
+    VERSION: '1.1.3+29.g9aed665d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
