af85ace77cd708bc1b98aadc0bd44a4fe1e9cb4e
Documentation Push <nipreps@gmail.com>
2020-03-05 20:10:39 +0000

    doc(update) [skip ci]

--- ci_docker-registry-caching/_static/documentation_options.js
+++ ci_docker-registry-caching/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1.9',
+    VERSION: '1.1.9+2.g76f831f1f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
