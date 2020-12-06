9ae03c57c1199494116ef32019c440645b22ae53
Documentation Push <nipreps@gmail.com>
2020-12-06 17:10:45 +0000

    doc(update) [skip ci]

--- 1.3.2/_static/documentation_options.js
+++ 1.3.2/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.3.2+28.g33cdb355e2',
+    VERSION: '1.3.2+29.gba8d4c8050',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
