cae27851c28875b895f6d03f5acf753fa111b750
Documentation Push <nipreps@gmail.com>
2021-02-04 22:49:31 +0000

    doc(update) [skip ci]

--- 1.2.9/_static/documentation_options.js
+++ 1.2.9/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.0rc2+68.g5c6f6e860c',
+    VERSION: '1.4.0rc2+69.ge2a86a43bd',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
