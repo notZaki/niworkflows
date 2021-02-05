f1674528a48f1ee6d07e80401e97e31e33f8a215
Documentation Push <nipreps@gmail.com>
2021-02-05 22:38:58 +0000

    doc(update) [skip ci]

--- 1.3.2/_static/documentation_options.js
+++ 1.3.2/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.0rc2+69.ge2a86a43bd',
+    VERSION: '1.4.0rc2+84.gc2d7fade51',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- enh_epi-reference-wf-issue-601/api/niworkflows-func-util-3.py
+++
@@ -1,2 +0,0 @@
-from niworkflows.func.util import init_skullstrip_bold_wf
-wf = init_skullstrip_bold_wf()
